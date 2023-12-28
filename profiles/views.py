774f811b925c311f6ff778a29f4c7d197b70f465
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-12-28 07:44:33 +0000

    feat(api): Add ProfileDetail APIView for retrieving, updating individual profiles

--- profiles/views.py
+++ profiles/views.py
@@ -11,3 +11,25 @@ class ProfileList(APIView):
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
 
+
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
