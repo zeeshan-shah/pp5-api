8dc606fcc26f4fdff8de3bf591f3a15045044cae
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-12-28 07:43:55 +0000

    feat(api): Add ProfileList APIView for retrieving all profiles

---
+++ profiles/views.py
@@ -0,0 +1,13 @@
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
+
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
