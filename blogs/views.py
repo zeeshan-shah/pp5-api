d57eb8bdf7c9c7d74bf8d8ae5f4d21fe9acdb666
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-12-28 13:01:32 +0000

    feat(api): Implement BlogList APIView for listing and creating blog posts

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Blog
+from .serializers import BlogSerializer
 
-# Create your views here.
+class BlogList(APIView):
+    serializer_class = BlogSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(
+            blogs, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BlogSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
