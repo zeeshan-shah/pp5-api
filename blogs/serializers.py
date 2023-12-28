7538d3b3fce621137b0bc012a24e99c3c08495b5
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-12-28 12:41:35 +0000

    feat(serializers): Add image validation and user profile details to BlogSerializer

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
-from posts.models import Post
+from posts.models import Blog  
 
-
-class PostSerializer(serializers.ModelSerializer):
+class BlogSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -12,21 +11,17 @@ class PostSerializer(serializers.ModelSerializer):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
         if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
+            raise serializers.ValidationError('Image height larger than 4096px!')
         if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
+            raise serializers.ValidationError('Image width larger than 4096px!')
         return value
 
     def get_is_owner(self, obj):
-        request = self.context['request']
+        request = self.context.get('request')
         return request.user == obj.owner
 
     class Meta:
-        model = Post
+        model = Blog  
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
