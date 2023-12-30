bae885d47c0702a744860bae05b9257c064d516c
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-12-30 19:57:02 +0000

    style(serializers): Improve CommentSerializer and CommentDetailSerializer

---
+++ comments/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from .models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id', allow_null=True)
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url', allow_null=True)
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context.get('request')
+        return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'blog', 'created_at', 'updated_at', 'content'
+        ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    blog = serializers.ReadOnlyField(source='blog.id', allow_null=True)
