9b04611d50a10cf06a4a3d54514e978addcab40c
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-12-30 20:00:31 +0000

    feat(api): Add FollowerList and FollowerDetail views with permissions for read-only and owner access

---
+++ followers/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics, permissions
+from pp5_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+class FollowerList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
