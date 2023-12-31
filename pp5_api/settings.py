429ec07f42eef06e9be2706e528c5998fd2549dd
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-12-31 16:36:07 +0000

    Fix: Procfile Bug fix

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'pp5-api-e56cee5dddfa.herokuapp.com']
+ALLOWED_HOSTS = ['8000-zeeshanshah-pp5api-97g36yogssc.ws-eu107.gitpod.io', 'pp5-api-e56cee5dddfa.herokuapp.com']
 
 # Application definition
 
