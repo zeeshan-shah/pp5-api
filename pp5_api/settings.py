84bc8b2782ac1d09154480c2360d68916eb0e282
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-12-31 13:44:25 +0000

    Fix: Herouku App name

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'pp5-api.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'pp5-api-e56cee5dddfa.herokuapp.com']
 
 # Application definition
 
