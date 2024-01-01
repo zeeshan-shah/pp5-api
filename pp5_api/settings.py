7e9d3354a16c9c8d8e74c34aea3ad624af13b5e8
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-01-01 20:37:35 +0000

    Chore: API setting for other project

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -65,7 +66,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-zeeshanshah-pp5api-97g36yogssc.ws-eu107.gitpod.io', 'pp5-api-e56cee5dddfa.herokuapp.com']
+ALLOWED_HOSTS = ['8000-zeeshanshah-pp5api-97g36yogssc.ws-eu107.gitpod.io', os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
@@ -113,9 +114,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
