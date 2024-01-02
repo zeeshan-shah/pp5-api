d33f1ba526b286290bd37d3ecdbb6c741bb39ca1
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-01-02 13:48:30 +0000

    Fix: URLs

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,5 +3,5 @@ from blogs import views
 
 urlpatterns = [
     path('blogs/', views.BlogList.as_view()),
-    path('blogs/<int:pk>', views.BlogDetail.as_view()),
+    path('blogs/<int:pk>/', views.BlogDetail.as_view()),
 ]
