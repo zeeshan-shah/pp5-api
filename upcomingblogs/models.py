3a776271dd9af3aecb98f6936fee8ce46089795f
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-02 18:26:16 +0000

    feat(models): Add UpcomingBlog model with release date validation

--- upcomingblogs/models.py
+++ upcomingblogs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class UpcomingBlog(models.Model):
     CATEGORY_CHOICES = [
@@ -17,6 +18,16 @@ class UpcomingBlog(models.Model):
     class Meta:
         ordering = ['-release_date']
 
-
     def __str__(self):
         return f'{self.title} by {self.owner.username}'
+
+    def release_date_has_passed(self):
+        today = timezone.now().date()
+        return self.release_date < today
+
+    def save(self, *args, **kwargs):
+        if self.release_date_has_passed():
+            # Delete the entry if the release date has passed
+            self.delete()
+        else:
+            super().save(*args, **kwargs)
