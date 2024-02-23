54da6423e834459201cfc83174867b41e3ad21f6
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-23 21:08:33 +0000

    feat: Implement Advertisement list API view

---
+++ advertisements/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+
+class AdvertisementListAPIView(generics.ListAPIView):
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
