7127c048ba83cee59a2730db019001c4c4db5480
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-23 21:06:46 +0000

    feat: Add Advertisement model

---
+++ advertisements/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Advertisement(models.Model):
+    """
+    Model representing an advertisement.
+    """
+    ADVERTISEMENT_TYPES = [
+        ('banner', 'Banner Ad'),
+        ('video', 'Video Ad'),
+        ('popup', 'Popup Ad'),
+        ('text', 'Text Ad'),
+    ]
+
+    title = models.CharField(max_length=255, verbose_name='Title')
+    advertisement_type = models.CharField(
+        max_length=20, choices=ADVERTISEMENT_TYPES, verbose_name='Ad Type'
+    )
+    image = models.ImageField(
+        upload_to='images/', default='default/ad-here.jpg',
+        verbose_name='Image'
+    )
+    video_url = models.URLField(
+        max_length=500, verbose_name='Video URL', null=True, blank=True
+    )
+    target_url = models.URLField(max_length=500, verbose_name='Target URL')
+    start_date = models.DateField(verbose_name='Start Date')
+    end_date = models.DateField(verbose_name='End Date')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Advertisement'
+        verbose_name_plural = 'Advertisements'
+
+    def __str__(self):
+        """
+        String for representing the Advertisement object.
+        """
+        return self.title
