7d2d77a76b518f9a3ef954b1515dddb0036ba6fd
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:56:47 +0000

    Refactor: Update UpcomingBlogSerializer

--- upcomingblogs/serializers.py
+++ upcomingblogs/serializers.py
@@ -6,22 +6,28 @@ class UpcomingBlogSerializer(serializers.ModelSerializer):
     """
     Serializer for the UpcomingBlog model.
     """
-    owner = serializers.ReadOnlyField(source='owner.username')
+
+    owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         """
         Check if the requesting user is the owner of the blog.
         """
-        request = self.context.get('request')
+        request = self.context.get("request")
         return request.user == obj.owner
 
     class Meta:
         """
         Meta class for UpcomingBlogSerializer.
         """
+
         model = UpcomingBlog
         fields = [
-            'id', 'owner', 'is_owner', 'title',
-            'category', 'release_date'
+            "id",
+            "owner",
+            "is_owner",
+            "title",
+            "category",
+            "release_date",
         ]
