9973211bc04cb974483a9b1df591321fb68cadbe
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:41:06 +0000

    style: Format urlpatterns in contact urls.py according to PEP 8

--- contact/urls.py
+++ contact/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from .views import ContactTicketListCreateView, ContactTicketDetailView
 
 urlpatterns = [
-    path('tickets/', ContactTicketListCreateView.as_view(),
-         name='contact-ticket-list-create'),
-    path('tickets/<int:pk>/', ContactTicketDetailView.as_view(),
-         name='contact-ticket-detail'),
+    path(
+        "tickets/",
+        ContactTicketListCreateView.as_view(),
+        name="contact-ticket-list-create",
+    ),
+    path(
+        "tickets/<int:pk>/",
+        ContactTicketDetailView.as_view(),
+        name="contact-ticket-detail",
+    ),
 ]
