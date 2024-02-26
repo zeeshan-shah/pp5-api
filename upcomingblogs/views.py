91f03ea2ead8a804ab515f3581d1b45ec3190561
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 22:09:02 +0000

    doc: Add docstrings to UpcomingBlogList and UpcomingBlogDetail

--- upcomingblogs/views.py
+++ upcomingblogs/views.py
@@ -6,22 +6,21 @@ from pp5_api.permissions import IsOwnerOrReadOnly
 
 class UpcomingBlogList(generics.ListCreateAPIView):
     """
-    API endpoint that allows creation and listing of upcoming blogs.
+    API endpoint that allows creation and listing
+    of upcoming blogs.
     """
 
     serializer_class = UpcomingBlogSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        """
-        Retrieve all upcoming blogs.
-        """
+        """ Retrieve all upcoming blogs. """
+
         return UpcomingBlog.objects.all()
 
     def perform_create(self, serializer):
-        """
-        Create a new upcoming blog instance.
-        """
+        """ Create a new upcoming blog instance. """
+
         serializer.save(owner=self.request.user)
 
 
