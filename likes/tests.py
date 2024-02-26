b6d608234d27ddc0b126096f74189fb68c4eb5ec
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 21:17:45 +0000

    doc: Add docstring to likes tests

--- likes/tests.py
+++ likes/tests.py
@@ -7,7 +7,19 @@ from .models import Like
 
 
 class LikeTestCase(TestCase):
+    """
+    A TestCase class for testing
+    Like model and its views.
+    """
+
     def setUp(self):
+        """
+        Set up the test environment by creating
+        a test user, a test blog, authenticating
+        a test client with the test user, and
+        creating a sample Like instance.
+        """
+
         self.user = User.objects.create_user(
             username="testuser",
             email="testuser@example.com",
@@ -21,12 +33,19 @@ class LikeTestCase(TestCase):
         self.like = Like.objects.create(owner=self.user, blog=self.blog)
 
     def test_like_creation(self):
+        """ Test the creation of a Like instance. """
+
         self.assertEqual(Like.objects.count(), 1)
         like = Like.objects.get(id=1)
         self.assertEqual(like.owner, self.user)
         self.assertEqual(like.blog, self.blog)
 
     def test_like_list_create_view(self):
+        """
+        Test the list and create views
+        for Like instances.
+        """
+
         response = self.client.get("/likes/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data["results"]), 1)
@@ -36,6 +55,11 @@ class LikeTestCase(TestCase):
         self.assertEqual(response.data["results"][0]["blog"], self.blog.id)
 
     def test_like_detail_view(self):
+        """
+        Test the detail view for a
+        Like instance.
+        """
+
         response = self.client.get(f"/likes/{self.like.pk}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["owner"], self.user.username)
