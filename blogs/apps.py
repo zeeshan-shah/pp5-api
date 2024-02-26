265f68953f95d67109f6ccd29ddb219df1c7adef
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 21:06:42 +0000

    doc: Add docstring to BlogsConfig

--- blogs/apps.py
+++ blogs/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class BlogsConfig(AppConfig):
+    """
+    AppConfig class for the
+    'blogs' app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "blogs"
