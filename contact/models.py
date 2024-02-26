854dc77a10b71767cf30859cb7dfe10b5f22ab2c
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:40:00 +0000

    style: Format ContactTicket model according to PEP 8

--- contact/models.py
+++ contact/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 TICKET_STATUSES = (
-    ('0', 'Pending'),
-    ('1', 'In Progress'),
-    ('2', 'Resolved'),
-    ('3', 'On Hold'),
+    ("0", "Pending"),
+    ("1", "In Progress"),
+    ("2", "Resolved"),
+    ("3", "On Hold"),
 )
 
 CONTACT_CATEGORIES = (
-    ('1', 'Blog Inquiry'),
-    ('2', 'Technical Support'),
-    ('3', 'Business Partnership'),
-    ('4', 'General Inquiry'),
-    ('5', 'Advertise with Us'),
+    ("1", "Blog Inquiry"),
+    ("2", "Technical Support"),
+    ("3", "Business Partnership"),
+    ("4", "General Inquiry"),
+    ("5", "Advertise with Us"),
 )
 
 
@@ -21,22 +21,25 @@ class ContactTicket(models.Model):
     """
     Model representing a contact ticket.
     """
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.CharField(
-        max_length=1, choices=CONTACT_CATEGORIES, default='1')
+        max_length=1, choices=CONTACT_CATEGORIES, default="1"
+    )
     subject = models.CharField(max_length=255)
     message = models.TextField(max_length=2000)
     ticket_status = models.CharField(
-        max_length=1, choices=TICKET_STATUSES, default='0')
+        max_length=1, choices=TICKET_STATUSES, default="0"
+    )
     admin_response = models.TextField(blank=True, null=True)
 
     class Meta:
-        ordering = ['-updated_at']
+        ordering = ["-updated_at"]
 
     def __str__(self):
         """
         String representation of the contact ticket.
         """
-        return f'Contact Ticket #{self.id} - {self.subject}'
+        return f"Contact Ticket #{self.id} - {self.subject}"
