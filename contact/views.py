af74679e07a8da2060f1639304c0906e8e5e2396
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 21:13:34 +0000

    doc: Add docstring to contact views

--- contact/views.py
+++ contact/views.py
@@ -5,36 +5,38 @@ from pp5_api.permissions import IsOwnerOrReadOnly
 
 
 class ContactTicketListCreateView(generics.ListCreateAPIView):
-    """
-    List or create contact tickets.
-    """
+    """ List or create contact tickets. """
 
     serializer_class = ContactTicketSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         """
-        Get the queryset of contact tickets owned by the current user.
+        Get the queryset of contact tickets
+        owned by the current user.
         """
+
         return ContactTicket.objects.filter(owner=self.request.user)
 
     def perform_create(self, serializer):
         """
-        Save the contact ticket with the current user as the owner.
+        Save the contact ticket with the
+        current user as the owner.
         """
+
         serializer.save(owner=self.request.user)
 
 
 class ContactTicketDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve, update or delete a contact ticket if the user owns it.
+    Retrieve, update or delete a contact
+    ticket if the user owns it.
     """
 
     serializer_class = ContactTicketSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
     def get_queryset(self):
-        """
-        Get the queryset of all contact tickets.
-        """
+        """ Get the queryset of all contact tickets. """
+
         return ContactTicket.objects.all()
