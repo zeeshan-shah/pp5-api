b955e7df3b44c3de4f9aaee969be5e3da1c1efc2
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:56:19 +0000

    Refactor: Update UpcomingBlog model

--- upcomingblogs/models.py
+++ upcomingblogs/models.py
@@ -7,30 +7,33 @@ class UpcomingBlog(models.Model):
     """
     Model representing upcoming blogs.
     """
+
     CATEGORY_CHOICES = [
-        ('science', 'Science and Technology'),
-        ('politics', 'Politics'),
-        ('sports', 'Sports'),
-        ('travel', 'Travel'),
-        ('programming', 'Programming'),
+        ("science", "Science and Technology"),
+        ("politics", "Politics"),
+        ("sports", "Sports"),
+        ("travel", "Travel"),
+        ("programming", "Programming"),
     ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     category = models.CharField(
-        max_length=20, choices=CATEGORY_CHOICES, verbose_name='Category')
+        max_length=20, choices=CATEGORY_CHOICES, verbose_name="Category"
+    )
     release_date = models.DateField()
 
     class Meta:
         """
         Meta class for UpcomingBlog model.
         """
-        ordering = ['release_date']
+
+        ordering = ["release_date"]
 
     def __str__(self):
         """
         String representation of the UpcomingBlog object.
         """
-        return f'{self.title} by {self.owner.username}'
+        return f"{self.title} by {self.owner.username}"
 
     def release_date_has_passed(self):
         """
