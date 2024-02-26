754e74c834fda6522d67504a109aba6b30092345
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:33:29 +0000

    style: Format URLs in urls.py according to PEP 8

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,11 +4,11 @@ from django.urls import path
 from blogs.views import BlogList, BlogDetail, BlogCategoryList
 
 urlpatterns = [
-    path('blogs/', BlogCategoryList.as_view(), name='blog-categories'),
-    path('blogs/<str:category>/', BlogList.as_view(), name='blog-list'),
+    path("blogs/", BlogCategoryList.as_view(), name="blog-categories"),
+    path("blogs/<str:category>/", BlogList.as_view(), name="blog-list"),
     path(
-        'blogs/<str:category>/<int:pk>/',
+        "blogs/<str:category>/<int:pk>/",
         BlogDetail.as_view(),
-        name='blog-detail'
+        name="blog-detail",
     ),  # URL pattern for a specific blog detail
 ]
