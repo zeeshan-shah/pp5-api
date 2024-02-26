cf873eb8a81fd2a75694504aecbe58989093e100
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:31:54 +0000

    style: Format Blog model according to PEP 8

--- blogs/models.py
+++ blogs/models.py
@@ -6,46 +6,50 @@ class Blog(models.Model):
     """
     Model representing a blog post.
     """
+
     CATEGORY_CHOICES = [
-        ('science', 'Science and Technology'),
-        ('politics', 'Politics'),
-        ('sports', 'Sports'),
-        ('travel', 'Travel'),
-        ('programming', 'Programming'),
+        ("science", "Science and Technology"),
+        ("politics", "Politics"),
+        ("sports", "Sports"),
+        ("travel", "Travel"),
+        ("programming", "Programming"),
     ]
 
     owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='blogs'
+        User, on_delete=models.CASCADE, related_name="blogs"
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255, verbose_name='Title')
-    description = models.TextField(max_length=500, verbose_name='Description')
-    content = models.TextField(max_length=4000, blank=True, verbose_name='Content')
+    title = models.CharField(max_length=255, verbose_name="Title")
+    description = models.TextField(max_length=500, verbose_name="Description")
+    content = models.TextField(
+        max_length=4000, blank=True, verbose_name="Content"
+    )
     image = models.ImageField(
-        upload_to='images/', default='../default-blog.png',
-        verbose_name='Image'
+        upload_to="images/",
+        default="../default-blog.png",
+        verbose_name="Image",
     )
     category = models.CharField(
-        max_length=20, choices=CATEGORY_CHOICES, verbose_name='Category'
+        max_length=20, choices=CATEGORY_CHOICES, verbose_name="Category"
     )
 
     class Meta:
-        ordering = ['-created_at']
-        verbose_name = 'Blog'
-        verbose_name_plural = 'Blogs'
+        ordering = ["-created_at"]
+        verbose_name = "Blog"
+        verbose_name_plural = "Blogs"
 
     def __str__(self):
         """
         String for representing the Blog object.
         """
-        return f'{self.title} by {self.owner.username}'
+        return f"{self.title} by {self.owner.username}"
 
     def get_short_description(self):
         """
         Return a short description for the blog post.
         """
         if len(self.description) > 100:
-            return self.description[:100] + '...'
+            return self.description[:100] + "..."
         else:
             return self.description
