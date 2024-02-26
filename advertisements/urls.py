93468fc04b328ca8ea515a047ff5b9d3b4c54ce4
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:28:44 +0000

    feat: Add URL configuration for AdvertisementListAPIView

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import AdvertisementListAPIView
 
 urlpatterns = [
-    path('advertisements/', AdvertisementListAPIView.as_view(), name='advertisement-list'),
+    path(
+        "advertisements/",
+        AdvertisementListAPIView.as_view(),
+        name="advertisement-list",
+    ),
 ]
