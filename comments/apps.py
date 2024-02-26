3862facff19e963e76fbeb69758469d1f3781eba
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 21:07:41 +0000

    doc: Add docstring to CommentsConfig

--- comments/apps.py
+++ comments/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class CommentsConfig(AppConfig):
+    """
+    AppConfig class for the
+    'comments' app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "comments"
