6a0f491bdb3ca32f9f89920c08162db89ae10423
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 21:06:13 +0000

    doc: Add docstring to AdvertisementsConfig

--- advertisements/apps.py
+++ advertisements/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class AdvertisementsConfig(AppConfig):
+    """
+    AppConfig class for the
+    'advertisements' app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "advertisements"
