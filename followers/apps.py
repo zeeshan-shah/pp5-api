36530dafebeabb7c648a541f3db20e2550d12d7d
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 21:14:23 +0000

    feat: Add AppConfig for the 'followers' app

--- followers/apps.py
+++ followers/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class FollowersConfig(AppConfig):
+    """
+    AppConfig class for the
+    'followers' app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "followers"
