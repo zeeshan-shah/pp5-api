0bd1eda848bc522bc78909b47ac68df46e051ce3
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:35:19 +0000

    style: Format Comment model according to PEP 8

--- comments/models.py
+++ comments/models.py
@@ -7,6 +7,7 @@ class Comment(models.Model):
     """
     Comment model, related to User and Blog models.
     """
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,7 +15,7 @@ class Comment(models.Model):
     content = models.TextField(max_length=500)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
 
     def __str__(self):
         return self.content
