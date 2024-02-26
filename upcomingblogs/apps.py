93b479fb3da86ae6a0f67aa843b2db877b0a32d1
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 22:07:09 +0000

    doc: Add docstring to UpcomingblogsConfig

--- upcomingblogs/apps.py
+++ upcomingblogs/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class UpcomingblogsConfig(AppConfig):
+    """
+    AppConfig class for the
+    'upcomingblogs' app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "upcomingblogs"
