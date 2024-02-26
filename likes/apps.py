ab021b9496ebc413691baa7a1db1dde36e0ba264
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 21:16:37 +0000

    feat: Add AppConfig for the 'likes' app

--- likes/apps.py
+++ likes/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class LikesConfig(AppConfig):
+    """
+    AppConfig class for the
+    'likes' app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "likes"
