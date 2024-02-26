ec96562b7266a9842f11cd9e65aa2a22a0db08ba
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:50:08 +0000

    style: Format urlpatterns in urls.py according to PEP 8

--- pp5_api/urls.py
+++ pp5_api/urls.py
@@ -13,23 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from .views import root_route, logout_route
 
 urlpatterns = [
-    path('', root_route),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('', include('profiles.urls')),
-    path('', include('blogs.urls')),
-    path('', include('comments.urls')),
-    path('', include('likes.urls')),
-    path('', include('followers.urls')),
-    path('', include('upcomingblogs.urls')),
-    path('', include('contact.urls')),
-    path('', include('advertisements.urls')),
+    path("", root_route),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("dj-rest-auth/logout/", logout_route),
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path(
+        "dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")
+    ),
+    path("", include("profiles.urls")),
+    path("", include("blogs.urls")),
+    path("", include("comments.urls")),
+    path("", include("likes.urls")),
+    path("", include("followers.urls")),
+    path("", include("upcomingblogs.urls")),
+    path("", include("contact.urls")),
+    path("", include("advertisements.urls")),
 ]
