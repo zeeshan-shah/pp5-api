cbf88abbd5de868c3863660ae7971bbc4657acfa
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 22:04:23 +0000

    doc: Add docstring to AppConfig class for the 'profiles' app

--- profiles/apps.py
+++ profiles/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
+    """
+    AppConfig class for the
+    'profiles' app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "profiles"
