4da62474ebcd33d10155ef4e646a48899570eb13
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 13:42:49 +0000

    style: Format Follower model according to PEP 8

--- followers/models.py
+++ followers/models.py
@@ -6,11 +6,12 @@ class Follower(models.Model):
     """
     Model representing followers/following relationship between users.
     """
+
     owner = models.ForeignKey(
-        User, related_name='following', on_delete=models.CASCADE
+        User, related_name="following", on_delete=models.CASCADE
     )
     followed = models.ForeignKey(
-        User, related_name='followed', on_delete=models.CASCADE
+        User, related_name="followed", on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -18,11 +19,12 @@ class Follower(models.Model):
         """
         Meta class for Follower model.
         """
-        ordering = ['-created_at']
-        unique_together = ['owner', 'followed']
+
+        ordering = ["-created_at"]
+        unique_together = ["owner", "followed"]
 
     def __str__(self):
         """
         String representation of the Follower object.
         """
-        return f'{self.owner} {self.followed}'
+        return f"{self.owner} {self.followed}"
