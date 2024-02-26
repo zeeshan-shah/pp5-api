88f0bcee38ba2adc611df9835f097d51b255ff57
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 22:12:20 +0000

    doc: Add docstrings to CommentTestCase

--- comments/tests.py
+++ comments/tests.py
@@ -7,8 +7,22 @@ from .models import Comment
 
 
 class CommentTestCase(TestCase):
+    """
+    A TestCase class for testing the Comment model and its views.
+
+    Attributes:
+        user (User): A test user created for authentication purposes.
+        blog (Blog): A test blog created for associating with comments.
+        client (APIClient): A test client for making requests to the API.
+        comment (Comment): A test comment instance created for testing.
+    """
 
     def setUp(self):
+        """
+        Set up the test environment by creating a test user,
+        a test blog, a test client, and a sample Comment instance.
+        """
+
         self.user = User.objects.create_user(
             username="testuser", password="testpassword"
         )
@@ -24,11 +38,18 @@ class CommentTestCase(TestCase):
         )
 
     def test_comment_creation(self):
+        """ Test the creation of a Comment instance. """
+
         self.assertEqual(Comment.objects.count(), 1)
         comment = Comment.objects.get(id=1)
         self.assertEqual(comment.content, "Test comment content")
 
     def test_comment_list_create_view(self):
+        """
+        Test the list view for comments and ensure created
+        comment is present.
+        """
+
         response = self.client.get("/comments/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check if response data contains any items
@@ -38,6 +59,8 @@ class CommentTestCase(TestCase):
             )
 
     def test_comment_detail_view(self):
+        """ Test the detail view for a Comment instance. """
+        
         response = self.client.get(f"/comments/{self.comment.id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["content"], "Test comment content")
