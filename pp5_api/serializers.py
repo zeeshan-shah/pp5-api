6852d5e0ac9c0bd600f4fe37f5fc32449e9cbf04
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 21:19:02 +0000

    doc: Add docstring to CurrentUserSerializer

--- pp5_api/serializers.py
+++ pp5_api/serializers.py
@@ -3,10 +3,36 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
+    """
+    Serializer class for the current user with
+    additional profile information.
+
+    This serializer extends the UserDetailsSerializer
+    provided by dj_rest_auth to include additional
+    fields related to the user's profile.
+
+    Attributes:
+        profile_id (ReadOnlyField): A read-only field
+        representing the profile ID associated with
+        the current user.
+        profile_image (ReadOnlyField): A read-only
+        field representing the profile image URL
+        associated with the current user.
+    """
+
     profile_id = serializers.ReadOnlyField(source="profile.id")
     profile_image = serializers.ReadOnlyField(source="profile.image.url")
 
     class Meta(UserDetailsSerializer.Meta):
+        """
+        Meta class extending UserDetailsSerializer.Meta.
+
+        This class defines the fields to be included in
+        the serialized representation of the current user,
+        including the fields from UserDetailsSerializer.Meta
+        and the additional profile fields.
+        """
+
         fields = UserDetailsSerializer.Meta.fields + (
             "profile_id",
             "profile_image",
